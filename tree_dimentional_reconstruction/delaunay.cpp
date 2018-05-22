9a3ad5e0807e148cff16056bc46c3b8fe2745436
zenglingchang <zlc774709311@163.com>
2018-05-22 15:53:50 +0800

    加入了三角面筛选
    对三角面片进行筛选，主要记录平均3D/2D线段的比值，大于比值的面不予记录。

--- tree_dimentional_reconstruction/delaunay.cpp
+++ tree_dimentional_reconstruction/delaunay.cpp
@@ -2,22 +2,28 @@
 
 Delaunay::Delaunay(Rect rect)
 {
+    ratio=0;
     this->rect=rect;
     subdiv=cv::Subdiv2D(rect);
 }
 
-void Delaunay::insert(std::vector<Point2f> &Points)
+void Delaunay::insert(std::vector<Point2f> &Points,std::vector<Point3f> &ObPoints)
 {
+    ObjectPoints=ObPoints;
     for(int i=0;i<Points.size();i++){
         subdiv.insert(Points[i]);
         Map[pair<float,float>(Points[i].x,Points[i].y)]=i;
     }
 }
-void Delaunay::GetFace(std::vector<int> &FaceList)
+void Delaunay::GetFace(Mat &src, Mat &dst, std::vector<int> &FaceList)
 {
+    dst=src;
     std::vector<Vec6f> TriangleLists;
     subdiv.getTriangleList(TriangleLists);
     std::vector<cv::Point2f> pt(3);
+    int num[3];
+    double Size=TriangleLists.size()*3;
+//    qDebug()<<"Size: "<<Size;
     for(size_t i=0;i<TriangleLists.size();i++){
         Vec6f t=TriangleLists[i];
         pt[0]=cv::Point2f(t[0],t[1]);
@@ -25,11 +31,18 @@ void Delaunay::GetFace(std::vector<int> &FaceList)
         pt[2]=cv::Point2f(t[4],t[5]);
         if (rect.contains(pt[0]) && rect.contains(pt[1]) && rect.contains(pt[2]))
         {
-            FaceList.push_back(Map[pair<float,float>(pt[0].x,pt[0].y)]);
-            FaceList.push_back(Map[pair<float,float>(pt[1].x,pt[1].y)]);
-            FaceList.push_back(Map[pair<float,float>(pt[2].x,pt[2].y)]);
+            for(int j=0;j<3;j++)
+                num[j]=Map[pair<float,float>(pt[j].x,pt[j].y)];
+            for(int j=0;j<3;j++){
+                ratio+=d(ObjectPoints[num[j]],ObjectPoints[num[(j+1)%3]])/d(pt[j],pt[(j+1)%3])/Size;
+                FaceList.push_back(Map[pair<float,float>(pt[j].x,pt[j].y)]);
+            }
+            line(dst, pt[0], pt[1], cv::Scalar(255,255,255), 1, CV_AA, 0);
+            line(dst, pt[1], pt[2], cv::Scalar(255,255,255), 1, CV_AA, 0);
+            line(dst, pt[2], pt[0], cv::Scalar(255,255,255), 1, CV_AA, 0);
         }
     }
+//    qDebug()<<"Ratio: "<<ratio;
 }
 void Delaunay::drawDelaunay(Mat &src, Mat &dst, Scalar delaunay_color)
 {
@@ -49,4 +62,13 @@ void Delaunay::drawDelaunay(Mat &src, Mat &dst, Scalar delaunay_color)
             line(dst, pt[2], pt[0], delaunay_color, 1, CV_AA, 0);
         }
     }
+}
+float d(Point2f &p1, Point2f &p2)
+{
+    return std::sqrt((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y));
+}
+
+float d(Point3f &p1, Point3f &p2)
+{
+    return std::sqrt((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y)+(p1.z-p2.z)*(p1.z-p2.z));
 }
--- tree_dimentional_reconstruction/delaunay.h
+++ tree_dimentional_reconstruction/delaunay.h
@@ -4,14 +4,18 @@
 
 class Delaunay
 {
+    double ratio;
     cv::Subdiv2D subdiv;
     cv::Rect rect;
     map<pair<float,float>,int> Map;
+    std::vector<Point3f> ObjectPoints;
 public:
     Delaunay(cv::Rect rect);
-    void insert(std::vector<cv::Point2f> &Points);
+    void insert(std::vector<cv::Point2f> &Points,std::vector<cv::Point3f> &ObPoints);
     void drawDelaunay(cv::Mat &src,cv::Mat &dst,Scalar);
-    void GetFace(std::vector<int> &FaceList);
+    void GetFace(cv::Mat &src,cv::Mat &dst,std::vector<int> &FaceList);
+    double GetRatio(){return ratio;}
 };
-
+float d(Point2f &p1,Point2f &p2);
+float d(Point3f &p1,Point3f &p2);
 #endif // DELAUNAY_H
